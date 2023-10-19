5a355996801cba767c4f9751a6650276634ae100
Kris Albert Lee <krisalbert@gmail.com>
2023-10-19 20:06:40 +0200

    update ArtistName, move getSeparator into its own function

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,15 +1,37 @@
-import { PropTypes } from "prop-types";
+import { PropTypes } from 'prop-types';
 
-import { AlbumName } from "./AlbumName";
-import { ArtistName } from "./ArtistName";
-import { CoverImage } from "./CoverImage";
+import { AlbumName } from './AlbumName';
+import { ArtistName } from './ArtistName';
+import { CoverImage } from './CoverImage';
+
+// getSeparator is a helper function to get the proper separator between artist names.
+// It can return the following results:
+// 1. "," if there are multiple artists, except for the last two artists
+// 2. "&" if there are only 2 artists, or between the last 2 artists if there are multiple artists
+// 3. "" (blank) if there is only one artist, or for the final artist on a list
+const getSeparator = (idx, arrayLength) => {
+  let separator = '';
+  if (idx === arrayLength - 2) {
+    separator = ' & ';
+  } else if (idx < arrayLength - 2) {
+    separator = ', ';
+  }
+  return separator;
+};
 
 export const Album = ({ album }) => {
   return (
     <div className="album-container">
       <CoverImage images={album.images} />
       <AlbumName albumName={album.name} link={album.external_urls.spotify} />
-      <ArtistName artists={album.artists} />
+      {album.artists.map((artist, idx) => {
+        return (
+          <span key={artist.id} className="artist-name">
+            <ArtistName artist={artist} />
+            {getSeparator(idx, album.artists.length)}
+          </span>
+        );
+      })}
     </div>
   );
 };
--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -1,29 +1,13 @@
-import { PropTypes } from "prop-types";
+import { PropTypes } from 'prop-types';
 
-export const ArtistName = ({ artists }) => {
+export const ArtistName = ({ artist }) => {
   return (
-    <div className="artist-name">
-      {artists.map((artist, idx, arr) => {
-        let separator = "";
-        if (idx === arr.length - 2) {
-          separator = " & ";
-        } else if (idx < arr.length - 2) {
-          separator = ", ";
-        }
-
-        return (
-          <span key={artist.id}>
-            <a href={artist.external_urls.spotify} target={`_blank`}>
-              {artist.name}
-            </a>
-            {separator}
-          </span>
-        );
-      })}
-    </div>
+    <a href={artist.external_urls.spotify} target={`_blank`}>
+      {artist.name}
+    </a>
   );
 };
 
 ArtistName.propTypes = {
-  artists: PropTypes.array.isRequired,
+  artist: PropTypes.object.isRequired,
 };
