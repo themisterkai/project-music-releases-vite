6f8bed0823fbcff17e865fae752e2b60d56e6a26
Kris Albert Lee <krisalbert@gmail.com>
2023-10-19 23:16:50 +0200

    update playlist

--- src/components/Sidebar/Playlist.jsx
+++ src/components/Sidebar/Playlist.jsx
@@ -7,12 +7,14 @@ import { PlaylistDescription } from './PlaylistDescription';
 export const Playlist = ({ playlist }) => {
   return (
     <div className="playlist-container">
-      <CoverImage imageSrc={playlist.images[0].url} withHover />
-      <AlbumName
-        albumName={playlist.name}
-        link={playlist.external_urls.spotify}
-      />
-      <PlaylistDescription description={playlist.description} />
+      <CoverImage imageSrc={playlist.images[0].url} />
+      <div className="playlist-text">
+        <AlbumName
+          albumName={playlist.name}
+          link={playlist.external_urls.spotify}
+        />
+        <PlaylistDescription description={`${playlist.tracks.total} tracks`} />
+      </div>
     </div>
   );
 };
